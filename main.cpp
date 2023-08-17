2d639802cdf0aa719bcc15150596eaea1a5f9388
RahulNavneeth <rahulmnavneeth@gmail.com>
2023-08-17 08:37:31 +0530

    src

--- main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-  std::cout << "HELLO WORLD! FROM RAHUL M. NAVNEETH" << std::endl;
-  return 0;
-}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
+use chrono;
 use std::io::Result;
 use std::process::Command;
 use std::time::Instant;
 
 fn main() -> Result<()> {
-    if std::path::Path::new("main.cpp").exists() {
+    let r: i32 = chrono::offset::Local::now().timestamp_millis() as i32;
+    if std::path::Path::new("src/main.cpp").exists() {
+        if !std::path::Path::new("exe").exists() {
+            std::fs::create_dir("exe")?;
+        }
         let compile_status = Command::new("g++")
             .arg("-std=c++11")
             .arg("-o")
-            .arg("main")
-            .arg("main.cpp")
+            .arg(format!("exe/main-{}", r))
+            .arg("src/main.cpp")
             .status();
 
         if let Ok(status) = compile_status {
             if status.success() {
-                let instance = Instant::now();
-                execute_main_executable();
-                println!("{:.2?}", instance.elapsed());
+                execute_main_executable(r);
             } else {
                 println!("COMPILATION FAILED.");
             }
@@ -23,18 +26,21 @@ fn main() -> Result<()> {
             println!("ERROR WHILE COMPILING MAIN.CPP");
         }
     } else {
-        println!("ERROR: *MAIN.CPP* FILE NOT FOUND.");
+        println!("ERROR: *SRC/MAIN.CPP* FILE NOT FOUND.");
     }
     Ok(())
 }
 
-fn execute_main_executable() {
-    if std::path::Path::new("main").exists() {
-        let execute_status = Command::new("./main").status();
+fn execute_main_executable(id: i32) {
+    if std::path::Path::new(&format!("exe/main-{}", id)).exists() {
+        let instance = Instant::now();
+        let execute_status = Command::new(&format!("exe/main-{}", id)).status();
 
         if let Ok(status) = execute_status {
             if !status.success() {
                 println!("ERROR: FAILED TO EXECUTE THE *MAIN* EXECUTABLE.");
+            } else {
+                println!("{:.2?}", instance.elapsed());
             }
         } else {
             println!("ERROR WHILE EXECUTING *MAIN* EXECUTABLE");
